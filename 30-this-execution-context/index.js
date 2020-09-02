936cd6c88c1238596883ba014c886877b10be65f
Shivang Dave <daveshivang15@gmail.com>
2020-09-02 11:03:37 -0500

    Add this && execution context..

---
+++ 30-this-execution-context/index.js
@@ -0,0 +1,11 @@
+// const seal = {
+//     name: 'sealy',
+//     kiss: function () {
+//         console.log(`${this.name} from a rose`)
+//     }
+// }
+
+// seal.kiss()
+
+// var someMethod = seal.kiss
+// someMethod()
---
+++ 30-this-execution-context/self.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Person
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    
+  end
+
+  def self.say_something
+    puts "self is: #{self}"
+  end
+
+  def print_name
+    puts "self is: #{self}"
+  end
+
+end
+
+binding.pry
+
+andy = Person.new('andy')
+puts andy.name
